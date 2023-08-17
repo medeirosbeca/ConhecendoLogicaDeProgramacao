76057c822193e100f48ffbcea238c672b9f6b8db
medeirosbeca <142314420+medeirosbeca@users.noreply.github.com>
2023-08-17 00:55:17 +0000

    exercicios

---
+++ EXERCICIOS/listaEncadeada.js
@@ -0,0 +1,19 @@
+/* Crie uma lista encadeada em que cada elemento representa uma pessoa. Ela precisa conter informações
+como nome, idade e referência ao filho dela.*/
+
+class ListaEncadeada {
+    constructor(){
+        this.cabeca = null;
+    }
+// Inserir no inicio (Push)
+addFirst (Gilberto){
+}
+
+//Inserir no fim ()
+addLast(Filha){
+}
+
+
+
+
+}
