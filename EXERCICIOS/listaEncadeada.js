fd0fc3415b39deaea17caf1abac607d18357b12c
medeirosbeca <142314420+medeirosbeca@users.noreply.github.com>
2023-08-18 14:44:34 +0000

    lista

--- EXERCICIOS/listaEncadeada.js
+++ EXERCICIOS/listaEncadeada.js
@@ -1,19 +1,47 @@
 /* Crie uma lista encadeada em que cada elemento representa uma pessoa. Ela precisa conter informações
 como nome, idade e referência ao filho dela.*/
 
-class ListaEncadeada {
-    constructor(){
-        this.cabeca = null;
+
+//Estrutura do Nó:
+class ConstruindoNo {
+    constructor() {
+      this.value = null;
+      this.next = null;
     }
-// Inserir no inicio (Push)
-addFirst (Gilberto){
-}
+    getValue() {
+      return this.value;
+    }
+    setValue(value) {
+      this.value = value;
+    }
+    getNext() {
+      return this.next;
+    }
+    setNext(next) {
+      this.next = next;
+    }
+  }
 
-//Inserir no fim ()
-addLast(Filha){
-}
-
-
-
-
+//Estrutura da Lista:
+class ListaEncadeada {
+    constructor() {
+        this.head = null;
+    }
+    add(value) {
+        let node = new Node();
+        node.setValue(value);
+        node.setNext(this.head);
+        this.head = node;
+    }
+    toString() {
+        let sb = new StringBuffer();
+        sb.append("[");
+        let ponteiro = this.head;
+        while (ponteiro !== null) {
+            sb.append(ponteiro.getValue() + " ");
+            ponteiro = ponteiro.getNext();
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
